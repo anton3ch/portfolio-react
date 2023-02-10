a22d555df16699266861e4c578e197e1b1a73d65
Anton Ch <43277455+anton3ch@users.noreply.github.com>
2023-02-09 23:19:46 -0600

    update ico and title

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,16 @@ import ProjectList from'./ProjectList.js';
 import Intro from'./Intro.js';
 import React from 'react';
 import Footer from './Footer';
+import { Helmet } from 'react-helmet';
 
 
 
 function App() {
   return (
     <div className="container pb-1">
+      <Helmet>
+        <title>Anton Ch</title>
+      </Helmet>
       <SidePanel />
       <Intro />
       <ProjectList />
