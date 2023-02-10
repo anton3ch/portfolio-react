00b5fb604578bed7e663e7afcef41921de1c9a59
Anton Ch <43277455+anton3ch@users.noreply.github.com>
2023-02-09 19:40:26 -0600

    resize project images

--- src/components/Project.js
+++ src/components/Project.js
@@ -8,15 +8,15 @@ function Project(props) {
       <div className="card card-s animate__animated animate__headShake">
         <div className="card-body">
           <h3 className="card-title text-center"><a href={props.deployedLink} target="_blank" rel="noreferrer">{props.projectName}</a></h3>
-          <a href={props.deployedLink} target="_blank" rel="noreferrer"> <img className="card-img ci1" src={props.img} onMouseOver={e => (e.currentTarget.src = props.imgGit)} onMouseOut={e => (e.currentTarget.src = props.img)} border="0" alt={props.imgAlt} /></a>
+          <a href={props.deployedLink} target="_blank" rel="noreferrer"> <img className="card-img ci" src={props.img} onMouseOver={e => (e.currentTarget.src = props.imgGit)} onMouseOut={e => (e.currentTarget.src = props.img)} border="0" alt={props.imgAlt} /></a>
           <h5 className="card-text">Description:</h5>
           <p className="card-text">{props.description}</p>
           <h5 className="card-text">Technologies Used:</h5>
-          <ol className="card-text">
+          <ul className="card-text">
           {props.technologies.map((technology, i) => {   
             return (<li key={i}><a href={technology.link} target="_blank" rel="noreferrer">{technology.name}</a></li>) 
           })}
-          </ol>
+          </ul>
           <p className="card-footer text-center"><a href={props.gitRepo} target="_blank" rel="noreferrer">github repository</a></p>
         </div>
       </div>
