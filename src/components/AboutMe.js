b3557b08e148e1fa858ae93ad22cdee312d06c10
Anton Ch <anton3ch@icloud.com>
2023-09-06 19:08:46 -0500

    change bio

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,23 +1,48 @@
-import React from 'react';
+import React from "react";
 import vampireKid from "./../img/vampire.png";
 
-export default function AboutMe(){
+export default function AboutMe() {
   return (
     <div className="about animate__animated animate__headShake mx-3">
-    <h2 className="mt-4 mb-2 heading2">About me</h2>
-    <hr/>
-    <div className="row mb-4">
-      <div className="col-md-12">
-        <div className="card">
-          <div className="card-body">
-            <p>My diverse background and passion for continuous learning bring a unique perspective to my work and enhance the quality of my projects. Having a passion for science and an unceasing thirst for knowledge, I am eager to tackle any challenge, whether working independently or as part of a team. I studied JavaScript, React, and C# at Epicodus, a highly intensive coding boot camp, where I built 60+ projects over a 27-week full-time program.
-
-            I am excited to continue growing my skills and making a positive impact in the technology industry. I am looking for opportunities to collaborate with like-minded individuals, tackle challenging projects, and contribute to shaping the future of technology for the betterment of all.</p>
-            <img className ="float" src={vampireKid} alt="vampire kid" />
+      <h2 className="mt-4 mb-2 heading2">About me</h2>
+      <hr />
+      <div className="row mb-4">
+        <div className="col-md-12">
+          <div className="card">
+            <div className="card-body">
+              <p>
+                I'm an inquisitive Software Engineer with a diverse background
+                and passion for science that fuels my enthusiasm for continuous
+                learning. My journey has granted me a unique perspective, which
+                I weave into my work to elevate project quality and discover
+                innovative solutions to complex problems. I eagerly embrace
+                challenges, whether navigating them independently or as an
+                integral part of a team. <br />
+                <br />
+                At PupPod, I'm currently engaged in developing and deploying a
+                cross-platform game app that caters to iOS, Android, and Web
+                users. By optimizing the app for different screen sizes, I've
+                ensured a seamless user experience across various devices. My
+                work also involves designing an intricate game progression
+                system with multiple levels, badges, and escalating challenges.
+                Additionally, I've contributed to the visual aspect of the app
+                by creating and enhancing image and GIF assets. <br />
+                <br />
+                During my time at Business Solution Network, I initiated and
+                successfully executed the migration of a legacy web application
+                to a modern React-based architecture. This transformation led to
+                a 50% reduction in page load times. My responsibilities extended
+                to designing and implementing dynamic employee onboarding
+                processes, leveraging JavaScript and CSS animations, thereby
+                boosting engagement by 40%. My commitment to responsive design
+                principles resulted in a 20% increase in mobile traffic and
+                ensured a consistent user experience across devices.
+              </p>
+              <img className="float" src={vampireKid} alt="vampire kid" />
+            </div>
           </div>
         </div>
       </div>
-    </div>  
-    </div>   
+    </div>
   );
 }
